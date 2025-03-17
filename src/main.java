96b092ba5e94ef0a70c8fc22d69b57a770507dcb
Lars <heijmen.lars@gmail.com>
2025-03-17 07:27:18 +0100

    Aded java file

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    @SuppressWarnings("MethodNameSameAsClassName")
+    public static void main(String[] args) {
+        for (int i = 0; i < 1_000_000_000; i++) {
+            System.out.println(i);
+        }
+    }
+}
